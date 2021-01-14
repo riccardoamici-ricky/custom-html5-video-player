22699b4c6b9dc80f0d64602715c434ffaa85f24b
riccardo amici <amici95@gmail.com>
2021-01-14 22:59:23 +0100

    initial js

---
+++ myscript.js
@@ -0,0 +1,11 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress_filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player_slider');
+
+function togglePlay() {
+	
+}
